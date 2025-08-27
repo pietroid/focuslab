1922ccf27e7ac4bb108453c9721204387a48eec2
pietroid <pietrodomingues@gmail.com>
2025-08-27 20:28:09 -0300

    v5: draft of focus home with recommendation cards

--- lib/new_home/new_home_page.dart
+++ lib/new_home/new_home_page.dart
@@ -25,7 +25,7 @@ class _NewHomePageState extends State<NewHomePage> {
       child: Stack(
         children: [
           Column(
-              mainAxisSize: MainAxisSize.min,
+              //mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 Column(
@@ -50,29 +50,44 @@ class _NewHomePageState extends State<NewHomePage> {
                         AnalogClock(),
                       ],
                     ),
-                    SizedBox(height: AppSpacing.large),
-                    DefaultCard(
-                        child: Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('Codando focuslab'),
-                              AddButton(),
-                            ],
+                    SizedBox(height: AppSpacing.small),
+                    Row(
+                      children: [
+                        DefaultCard(
+                          child: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Column(
+                              children: [
+                                Text('💰 Financeiro'),
+                                SizedBox(height: AppSpacing.small),
+                                Text('R\$ 200,00 de mercado até dia 30',
+                                    style: textTheme.bodySmall?.copyWith(
+                                      color: AppColors.captionColor,
+                                    )),
+                              ],
+                            ),
                           ),
-                          SizedBox(height: AppSpacing.small),
-                          ProgressBar(progress: 65, maxValue: 100),
-                          SizedBox(height: AppSpacing.small),
-                          Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: [
-                              Text('19:00 - 21:00'),
-                              Text('Faltam 1:20'),
-                            ],
-                          )
-                        ])),
+                        ),
+                        SizedBox(width: AppSpacing.small),
+                        DefaultCard(
+                          child: SizedBox(
+                            height: 100,
+                            width: 100,
+                            child: Column(
+                              children: [
+                                Text('🏃‍♂️ Saúde'),
+                                SizedBox(height: AppSpacing.small),
+                                Text('3 litros de água hoje',
+                                    style: textTheme.bodySmall?.copyWith(
+                                      color: AppColors.captionColor,
+                                    )),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
                     SizedBox(height: AppSpacing.large),
                     Text('Ainda hoje', style: textTheme.titleLarge),
                     SizedBox(height: AppSpacing.small),
@@ -93,26 +108,53 @@ class _NewHomePageState extends State<NewHomePage> {
                         Text('21:30', style: textTheme.bodySmall),
                       ],
                     )),
+                    SizedBox(height: AppSpacing.large),
                   ],
                 ),
               ]),
+          // Align(
+          //     alignment: Alignment.bottomRight,
+          //     child: GestureDetector(
+          //       onTap: () {
+          //         PopupPage.show(
+          //             context: context,
+          //             content: DevModePopupContent(
+          //               timeConstrainedNowTask: timeConstrainedNowTask,
+          //               onTimeConstrainedNowTaskChanged: (value) {
+          //                 setState(() {
+          //                   timeConstrainedNowTask = value;
+          //                 });
+          //               },
+          //             ));
+          //       },
+          //       child: const Icon(Icons.add),
+          //     )),
           Align(
-              alignment: Alignment.bottomRight,
-              child: GestureDetector(
-                onTap: () {
-                  PopupPage.show(
-                      context: context,
-                      content: DevModePopupContent(
-                        timeConstrainedNowTask: timeConstrainedNowTask,
-                        onTimeConstrainedNowTaskChanged: (value) {
-                          setState(() {
-                            timeConstrainedNowTask = value;
-                          });
-                        },
-                      ));
-                },
-                child: const Icon(Icons.add),
-              )),
+            alignment: Alignment.bottomCenter,
+            child: DefaultCard(
+                child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('Codando focuslab'),
+                      AddButton(),
+                    ],
+                  ),
+                  SizedBox(height: AppSpacing.small),
+                  ProgressBar(progress: 65, maxValue: 100),
+                  SizedBox(height: AppSpacing.small),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text('19:00 - 21:00'),
+                      Text('Faltam 1:20'),
+                    ],
+                  )
+                ])),
+          ),
         ],
       ),
     ));
